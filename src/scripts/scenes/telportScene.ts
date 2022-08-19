cbc07af2ed35b2966db510f6b12e844d4611f6b6
Mihai Vasilache <mvasilache@adobe.com>
2022-08-19 18:05:56 +0300

    Revert "Auxiliary commit to revert individual files from 95f998c6a9e1ec4b9e322c76a4f59497f4cea55a"
    This reverts commit 73d024eaaee0771027d99f717968384789ead251, reversing
    changes made to a0e7c30fa6a548243503c7976224c46c9e54fc36.
    
    # Conflicts:
    #	src/scripts/scenes/telportScene.ts
    #	src/scripts/sprites/grizzly.ts
    
    # Conflicts:
    #	src/scripts/scenes/telportScene.ts
    #	src/scripts/sprites/hero.ts

--- src/scripts/scenes/telportScene.ts
+++ src/scripts/scenes/telportScene.ts
@@ -1,8 +1,4 @@
 import { Scene } from 'phaser';
-<<<<<<< HEAD
-import Hero from '../sprites/hero';
-=======
->>>>>>> f77307134e20cf453bd5c24588b211530f4cd074
 import { PhaserNavMeshPlugin } from 'phaser-navmesh';
 import Hero from '../sprites/hero';
 
